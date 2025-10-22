9cab1d300f3f8763b3cfa4ba51ea08f4699f5edb
gretaancora <gretaluna.ancora@gmail.com>
2025-10-23 00:18:32 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <asm/prctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+#define READ_THREAD_VARIABLE(name) \
+({ \
+uint64_t v; \
+size_t off = offsetof(pthread_block, name); \
+asm volatile("movq %%gs:%1, %0" : "=r"(v) : "m"(*(uint64_t *)off)); \
+(typeof(((pthread_block*)0)->name))v; \
+})
+
+
+#define WRITE_THREAD_VARIABLE(name, value) \
+do { \
+size_t off = offsetof(pthread_block, name); \
+uint64_t v = (uint64_t)(value); \
+asm volatile("movq %0, %%gs:%1" : : "r"(v), "m"(*(uint64_t *)off)); \
+} while(0)
+
+
+#define PER_THREAD_MEMORY_START typedef struct pthread_block {
+
+#define PER_THREAD_MEMORY_END } pthread_block;
+
+
+PER_THREAD_MEMORY_START
+    int i;
+    short j;
+PER_THREAD_MEMORY_END
+
+void* the_thread(void* dummy) {
+
+    unsigned long me = (unsigned long)dummy;
+
+    //mappo area riservata del thread
+    void *thread_area = mmap(NULL, sizeof(pthread_block), PROT_WRITE|PROT_READ, MAP_ANONYMOUS| MAP_PRIVATE, 0 , 0);
+    if (thread_area==MAP_FAILED) {
+        printf("thread area install error\n");
+        exit(1);
+    }
+
+    //faccio puntare GS a tale area riservata
+    syscall(SYS_arch_prctl, ARCH_SET_GS, (unsigned long)thread_area);
+
+    if (me==0) {
+        WRITE_THREAD_VARIABLE(i, 3);
+        printf("thread %lu variable i: %d \n", me, READ_THREAD_VARIABLE(i));
+    }else {
+        WRITE_THREAD_VARIABLE(i, 11);
+        printf("thread %lu variable i: %d \n", me, READ_THREAD_VARIABLE(i));
+    }
+
+    return NULL;
+
+}
+
+
+
+
+int main(void) {
+
+    pthread_t tid;
+
+    pthread_create(&tid,NULL,the_thread,(void*)0);
+    pthread_create(&tid,NULL,the_thread,(void*)1);
+    pause();
+
+}
